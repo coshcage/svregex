84807eab815958997f20aa653d0eeeebaf919dab
coshcage <58808125+coshcage@users.noreply.github.com>
2023-12-10 03:54:23 -0800

    Add files via upload

--- src/svregex.h
+++ src/svregex.h
@@ -2,7 +2,7 @@
  * Name:        svregex.h
  * Description: SV Regular Expression Interface.
  * Author:      cosh.cage#hotmail.com
- * File ID:     1024231324B1210231300L00056
+ * File ID:     1024231324B1210231300L00060
  * License:     GPLv2.
  */
 #ifndef _SVREGEX_H_
@@ -20,7 +20,7 @@
 typedef P_MATRIX P_DFA; /* Redefine P_MATRIX. */
 
 void   PrintDFA         (P_DFA      pmtx);
-P_DFA  CompileRegex2DFA (wchar_t *  pwc);
+P_DFA  CompileRegex2DFA (wchar_t * pwc);
 void   DestroyDFA       (P_DFA      dfa);
 size_t NextState        (P_DFA      dfa, size_t s, wchar_t a);
 size_t NextStateM       (P_DFA      dfa, size_t s, wchar_t a);
@@ -31,25 +31,29 @@ P_DFA  MinimizeDFA      (P_DFA      dfa);
  * NextState runs a little bit faster than NextStateM.
  */
 
-/* Escape sequence that this library accepted.
- * \e = Epsilon.
- * \n
- * \t
- * \v
- * \r
- * \v
- * \f
- * \b
- * \(
- * \)
- * \\
- * \.
- * Operators:
- * |
- * .
- * *
- * (
- * )
- */
+ /* Escape sequence that this library accepted.
+  * \e = Epsilon.
+  * \n
+  * \t
+  * \v
+  * \r
+  * \v
+  * \f
+  * \b
+  * \(
+  * \)
+  * \\
+  * \.
+  * Operators:
+  * |
+  * .
+  * *
+  * (
+  * )
+  * Note that:
+  * a+ == aa*
+  * a? == a|\e
+  * (a+)? == (a?)+ == a*
+  */
 
 #endif
